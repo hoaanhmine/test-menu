5ca0c0406e9c24716406ee602a4eeb27cc62ce85
HMFNF <51531073+hoaanhmine@users.noreply.github.com>
2022-11-25 20:06:11 +0700

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,8 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var curSelected:Int = 0;
+        public static var psychEngineVersion:String = '0.5.3'; //This is also used for Discord RPC
+ 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
 	private var camGame:FlxCamera;
@@ -171,8 +172,14 @@ class MainMenuState extends MusicBeatState
 			}
 		}
 		#end
+                
 
-		super.create();
+                #if android
+                addVirtualPad(UP_DOWN, A_B);
+                #end
+		
+
+                super.create();
 	}
            
 
@@ -185,9 +192,7 @@ class MainMenuState extends MusicBeatState
 	}
 	#end
             
-                #if android
-                addVirtualPad(UP_DOWN, A_B);
-                #end
+               
 
 	var selectedSomethin:Bool = false;
 
